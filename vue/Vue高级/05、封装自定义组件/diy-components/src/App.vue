24e1174c0e71394d1f056ace0009b92b0376ced7
wuzhe-redmi <wuzhe_@aliyun.com>
2021-05-27 23:21:42 +0800

    重新研究自定义的上传组件的代码

---
+++ vue/Vue高级/05、封装自定义组件/diy-components/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <Uploader v-model="imgsList">
+        <div>选择文件</div>
+      </Uploader>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+import Uploader from 'common/Uploader'
+export default {
+  data () {
+    return {
+      imgsList: []
+    }
+  },
+  components: {
+    Uploader
+  }
+}
+</script>
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ vue/Vue高级/05、封装自定义组件/diy-components/vue.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, 'src', dir)
+}
+module.exports = {
+  chainWebpack: function(config) {
+    config.resolve.alias.set('common', resolve('components/common'))
+  }
+
+
+}
